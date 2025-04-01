d57b8353cbcefade66fcf83dceb91a70182198d9
Kunbam7 <kunh9905@gmail.com>
2025-04-01 10:13:09 +0900

    removed .DS_Store

---
+++ html/tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                secondary: '#ffed4a',
+                danger: '#e3342f',
+                clifford: '#da373d',
+                carrot: '#e67e22',
+                main: '#00d8d6',
+                river: '#3498db',
+                primary: '#3490dc',
+                favortie: '#74b9ff',
+            },
+        },
+    },
+};
