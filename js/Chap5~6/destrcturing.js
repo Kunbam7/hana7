0a175a369919cb19e8bc7a328b2ae682d10d4179
Kunbam7 <kunh9905@gmail.com>
2025-04-08 08:48:11 +0900

    homework

--- js/Chap5~6/destrcturing.js
+++ js/Chap5~6/destrcturing.js
@@ -1,2 +1,6 @@
 const { id, name = 'Hong', addr = 'xx' } = { id: 1, name: undefined, addr: null };
 console.log(name);
+
+const arr = [1,2,[3,4],[5,6],{ax:7, ay:8}, {ax:9}];
+const [,x,[,y],z,,{ax}] = arr;
+console.log(x);
--- js/Chap5~6/ex_chap6.js
+++ js/Chap5~6/ex_chap6.js
@@ -3,31 +3,29 @@ const user = {id : 1, name : 'Hong', addr : {city : 'Seoul'}};
 const hong = {id : 1, name : 'Hong'};
 const lee = {id : 2, name : 'Lee'};
 
-function f1() {
-
+function f1(list) {
+    let test = list;
+    console.log(test.id, test.name);
 }
-function f2() {
-
+function f2(list) {
+    const {id, name} = list;
+    console.log(id, name);
 }
-f1(hong)
-f2(hong)
+f1(user)
+f2(user)
 // 두 결과값 모두 동일하게 1, Hong or 2, Lee
 
 console.log('==================');
 
 // passwd 외 가져오기, rest 연산자 사용?
 const user2 = {id : 1, name : 'Hong', passwd : 'xxx', addr : 'Seoul'};
-const userInfo = {...user2};
+const {passwd, ...userInfo} = user2;
 console.log(userInfo);
 // {id : 1, name : 'Hong', addr: 'Seoul}
-
 console.log('==================');
 
 // 3개 id 각각 할당
 const arr = [[{id : 1}], [{id : 2}, {id : 3}]];
-
-let id1, id2, id3;  //임시조치
-
-
+const [[{id : id1}], [{id : id2}, {id : id3}]] = arr;
 console.log(id1, id2, id3);
 // 1 2 3
