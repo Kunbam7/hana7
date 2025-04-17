da2a636619b35019a0786a103a7c6de1fface978
Kunbam7 <kunh9905@gmail.com>
2025-04-17 10:30:59 +0900

    17 sol

--- js/Chap14~17/Chap17_ex.js
+++ js/Chap14~17/Chap17_ex.js
@@ -16,18 +16,45 @@ console.log('low: ', low);
 
 // 1-2
 const assert = require('assert');
-const swapCase = str => 
+const swapCase = str => {
+    const check = /[A-Z]*/g;
+    for(let i = 0; i < str.length; i+= 1) {
+        //check.str[i] ? str[i].toUpperCase() : str[i].toLowerCase();
+
+        // const result = check.str ? str.toUpperCase() : str.toLowerCase();
+        // return result;
+    }
+    //return str;
+    
+
+
+    // do-while 써야하나?
+    // const lowerWords = str(/[A-Z]/);
+    // const upperWords = str(/[a-z]/);
+    // console.log('lower: ', lowerWords);
+    // console.og('upper: ', upperWords);
+    //쪼갤거 없이, 형식지키는지 파악하는 함수 만들고, 그걸 str에 씌우는식으로 하기?
+    // /[A-Z]/ ? lower : upper -> if A-Z? -> lower ifnot -> upper
+    // if() {
+    //     str.replace(/[A-Z]/g, foundStr => foundStr.toLowerCase();)
+    // }
+}
 
 assert.equal(swapCase('Senior Coding Learning JS'), 'sENIOR cODING lEARNING js');
 assert.equal(swapCase('Hanaro 4 Class'), 'hANARO 4 cLASS');
 
 
 // 2
-assert.deepStrictEqual(telfmt('0101234567'), '010-123-4567');
-assert.deepStrictEqual(telfmt('01012345678'), '010-1234-5678');
-assert.deepStrictEqual(telfmt('0212345678'), '02-1234-5678');
-assert.deepStrictEqual(telfmt('021234567'), '02-123-4567');
-assert.deepStrictEqual(telfmt('0331234567'), '033-123-4567');
-assert.deepStrictEqual(telfmt('15771577'), '1577-1577');
-assert.deepStrictEqual(telfmt('07012341234'), '070-1234-1234');
-assert.deepStrictEqual(telfmt('050712345678'), '0507-1234-5678');
+
+// assert.deepStrictEqual(telfmt('0101234567'), '010-123-4567');
+// assert.deepStrictEqual(telfmt('01012345678'), '010-1234-5678');
+// assert.deepStrictEqual(telfmt('0212345678'), '02-1234-5678');
+// assert.deepStrictEqual(telfmt('021234567'), '02-123-4567');
+// assert.deepStrictEqual(telfmt('0331234567'), '033-123-4567');
+// assert.deepStrictEqual(telfmt('15771577'), '1577-1577');
+// assert.deepStrictEqual(telfmt('07012341234'), '070-1234-1234');
+// assert.deepStrictEqual(telfmt('050712345678'), '0507-1234-5678');
+
+// 케이스 쪼개다가 if굴리기
+// 1. 갯수
+// 2. 
--- js/Chap14~17/Chap17_ex_sol.js
+++ js/Chap14~17/Chap17_ex_sol.js
@@ -16,7 +16,49 @@ console.log('low: ', low);
 
 // 1-2
 const assert = require('assert');
-const swapCase = str => 
+const { TransformStreamDefaultController } = require('stream/web');
+const swapCase = str =>
+    str.replace(/([A-Z\s\d]*)([a-z]*)/g,
+        //? : null이 있을가능성때문, * : 대문자나 소문자가 없을 가능성, 순서가 대-소가 아닌 경우 고려
+        (foundStr, upper, lower) => { 
+            if (!foundStr?.trim()) return '';
+            return `${upper.toLowerCase()}${lower.toUpperCase()}`;
+        });
+
+const swapCaseSol = str =>
+    str?.replace(/([A-Z]+)([a-z]*)/g,
+        //? : null이 있을가능성때문, * : 대문자나 소문자가 없을 가능성, 순서가 대-소가 아닌 경우 고려
+        (upper, lower) => `${upper.toLowerCase()}${lower.toUpperCase()}`);    
+
+const swapCase2 = str => str?.replace(/([A-Z\s]*)([a-z]*)/g
+    , (founStr, upper, lower) => {
+        return `${upper.toLowerCase()}${lower.toUpperCase()}`;
+    });
 
 assert.equal(swapCase('Senior Coding Learning JS'), 'sENIOR cODING lEARNING js');
 assert.equal(swapCase('Hanaro 4 Class'), 'hANARO 4 cLASS');
+assert.equal(swapCase('test For starting Low words'), 'TEST fOR STARTING lOW WORDS');
+
+
+// 2
+const telfmt = telno => {
+    const len = telno?.length ?? 0;
+    if(len <= 7)    return telno;
+    // 8자 => 4-4
+    if(len === 8)   return `${telno.substring(0,4)}-${telno.substring(4)}`;
+
+    let a = telno.startsWith('02') ? 2 : len > 10 ? len - 8 : 3;
+    let b = len - a - 4;
+    // /abc/ = "abc"
+    const reg = new RegExp(`(\\d{${a}})(\\d{${b}})(\\d{4})`);
+    return telno.replace(reg, '$1-$2-$3');
+};
+
+assert.deepStrictEqual(telfmt('0101234567'), '010-123-4567');
+assert.deepStrictEqual(telfmt('01012345678'), '010-1234-5678');
+assert.deepStrictEqual(telfmt('0212345678'), '02-1234-5678');
+assert.deepStrictEqual(telfmt('021234567'), '02-123-4567');
+assert.deepStrictEqual(telfmt('0331234567'), '033-123-4567');
+assert.deepStrictEqual(telfmt('15771577'), '1577-1577');
+assert.deepStrictEqual(telfmt('07012341234'), '070-1234-1234');
+// assert.deepStrictEqual(telfmt('050712345678'), '0507-1234-5678');
