f8e60b192997cee721024fc2a1cb13c5249de19e
Kunbam7 <kunh9905@gmail.com>
2025-05-26 09:10:56 +0900

    img page

--- next/my-app/app/photos/page.tsx
+++ next/my-app/app/photos/page.tsx
@@ -1,17 +1,43 @@
-import Link from "next/link";
-import Image from "next/image";
-import { use } from "react";
+import Image from 'next/image';
+import Link from 'next/link';
+import { use } from 'react';
+
+type Props = {
+    id: string,
+    author: string,
+    download_url: string;
+};
 
 export default function Photos() {
-    const photos = use(fetch('https://picsum.photos/v2/list?limit=10').then( (res) => res.json()));
-    console.log(photos);
-    return <>
-        <h1 className="text-3xl">Photos</h1>
-        {photos.map(({id, author, download_url }) => <Link key = {id} href={`/photos/${id}`}>
-            <Image src={download_url} alt={author} width={150} height={150} quality={70} 
-            // priority -> lazy 반대
-            loading='lazy' placeholder="blur" blurDataURL="/next.svg"/>
-        
-        </Link>)}
-    </>;
-};
+  const photos = use(
+    fetch('https://picsum.photos/v2/list?limit=9').then((res) => res.json())
+  );
+
+  return (
+    <>
+      <h1 className='text-3xl'>Photos</h1>
+      <div className='flex flex-wrap justify-center gap-3'>
+        {photos.map(({ id, author, download_url }:Props) => (
+          <Link
+            key={id}
+            href={`/photos/${id}`}
+            className='opacity-80 hover:opacity-100 scale-95 hover:scale-100'
+          >
+            <Image
+              src={download_url}
+              alt={author}
+              width={150}
+              height={150}
+              quality={70}
+              // priority
+              loading='lazy'
+              placeholder='blur'
+              blurDataURL='/next.svg'
+              unoptimized
+            />
+          </Link>
+        ))}
+      </div>
+    </>
+  );
+}
