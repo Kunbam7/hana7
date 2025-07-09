29fb230c4087119ee3f916487fcbd760564ae200
Kunbam7 <kunh9905@gmail.com>
2025-07-09 17:12:33 +0900

    generic mention

--- Java/basic/src/oop/FruitComparator.java
+++ Java/basic/src/oop/FruitComparator.java
@@ -17,7 +17,7 @@ public class FruitComparator implements Comparator<Fruit> {
 		System.out.println(banana instanceof Fruit);
 		System.out.println(redApple instanceof Fruit);
 
-		int result1 = fc.compare(banana, redApple);    // 앞글자 아스키코드 값을 비교, 그 값을 +-한 값 출력
+		int result1 = fc.compare(banana, redApple);    // 앞글자부터 보고, 다른글자 발견하면 그 글자의 아스키코드 값을 비교, 그 값을 +-한 값 출력
 		int result2 = fc.compare(redApple, banana);    // -면 뒤가, +면 앞이 더 큰것(알파벳 순)
 		System.out.println("Result1: " + result1);
 		System.out.println("Result2: " + result2);
