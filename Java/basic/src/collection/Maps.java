08d488ca56729ed337f91d91e6ba67c7198c882e
Kunbam7 <kunh9905@gmail.com>
2025-07-11 15:47:55 +0900

    Map

---
+++ Java/basic/src/collection/Maps.java
@@ -0,0 +1,28 @@
+package collection;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import oop.Circle;
+
+public class Maps {
+	public static void main(String[] args) {
+		// Map<String, Circle> mapCircles = new HashMap<>();
+		Map<String, Circle> mapCircles = new TreeMap<>();    // key 정렬
+		mapCircles.put("C1", new Circle(25));
+		mapCircles.put("C2", new Circle(5));
+		mapCircles.put("C3", new Circle(50));
+		System.out.println("mapCircles = " + mapCircles);
+
+		Set<String> strings = mapCircles.keySet();
+		System.out.println("strings = " + strings);
+
+		Collection<Circle> values = mapCircles.values();
+		System.out.println("values = " + values);
+
+		Set<Map.Entry<String, Circle>> entries = mapCircles.entrySet();
+		System.out.println("entries = " + entries);
+	}
+}
