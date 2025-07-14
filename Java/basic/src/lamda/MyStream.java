a09686e2c83dc5eccb9432111dc466d40fbdddca
Kunbam7 <kunh9905@gmail.com>
2025-07-14 17:31:24 +0900

    Lamda-Stream

--- Java/basic/src/lamda/MyStream.java
+++ Java/basic/src/lamda/MyStream.java
@@ -26,13 +26,17 @@ public class MyStream {
 		System.out.println("처음 5개 건너뛰고 출력");
 		list.stream().skip(5).toList();
 		System.out.println("값이 5보다 큰 것만 출력");
-		// list.stream().filter(val -> val > 5).toList();	//me
-		list.stream().takeWhile(val -> val > 5).toList();    // 1
-		// list.stream().dropWhile(val -> val <= 5).toList();	// 2
+		list.stream().filter(val -> val > 5).toList();    //me
+		// takeWhile, dropWhile은 순서에 영향을 받음( 첫번째가 거짓일째, 이후 확인없이 종료
+		// list.stream().takeWhile(val -> val > 5).toList();    // 1
+		// list.stream().dropWhile(val -> val <= 5).toList();    // 2
+		System.out.println("list = " + list);
+		System.out.println("list = " + list);
 		System.out.println("1 ~ 10의 합계");
-		// stream은 완료된 상태가 아니기 때문에 바로 sum()사용 불가
-
+		// stream은 완료된 상태가 아니기 때문에 바로 sum()사용 불가?
+		// System.out.println(range1.sum());	// me, 1
+		list.stream().mapToInt(Integer::intValue).sum();    // 2
 		System.out.println("random 5개의 평균");
-
+		Stream.generate(Math::random).limit(5).mapToDouble(Double::doubleValue).average();
 	}
 }
