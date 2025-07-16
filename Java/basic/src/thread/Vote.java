433c673a0c95f7978771e707dc844438eaacd08b
Kunbam7 <kunh9905@gmail.com>
2025-07-16 17:28:48 +0900

    self feedback vote from class

--- Java/basic/src/thread/Vote.java
+++ Java/basic/src/thread/Vote.java
@@ -1,25 +1,34 @@
 package thread;
 
 public class Vote implements Runnable {
-	int rate = 0;
 
+	// 람다식으로 간단히 표현 가능
 	@Override
 	public void run() {
-		String name = Thread.currentThread().getName();
+		String name = Thread.currentThread().getName(); // -> 요건 굿?
+		// 굳이 없어도 되는 변수 -> souf에 박아넣음 됨
 		String bar = "*";
+		int rate = 0;
+		// 굳이 for를 빠로 만들필요없이, while 구문으로 rate 하나로 루프 굴리는 방식으로, rate 100 초과를 신경쓰지 않는 방향으로
 		for (int i = 0; i < 100; i++) {
+			// Math.random 저건 0 ~ 0.9 사이값 출력하는걸 기억해둘것
 			int instanceRate = (int)(Math.random() * 5) + 1;
 			rate += instanceRate;
 
-			System.out.printf("제%s 개표율: %d%(개표증가율 : %d%)%s%n", name, rate, instanceRate, bar);
+			// 다른방법 생각할거 없이 .repeat()으로 계속 커지게 만듦됬었음
+			// 숫자 뒤 % 붙이려면 %%으로 해야함
+			System.out.printf("%s 개표율: %d%%(개표증가율 : %d%%)%s%n", name, rate, instanceRate, bar.repeat(rate));
 
 			try {
-				int millis = 100; // (int)(Math.random() * 100) + 1;
+				// 한번 쓸놈인데, 굳이 변수만들필요 없었음
+				int millis = (int)(Math.random() * 1000);
 				Thread.sleep(millis);
-			} catch (InterruptedException e) {
+			}
+			// 걍 리턴으로 종료시키는게 나음
+			catch (InterruptedException e) {
 				throw new RuntimeException(e);
 			}
-
+			// 왜 여따 넣었드라...?
 			if (rate >= 100) {
 
 			}
@@ -27,8 +36,9 @@ public class Vote implements Runnable {
 	}
 
 	public static void main(String[] args) {
-		Vote area1 = new Vote();
+		Thread area1 = new Thread("제1지역구");
 		Vote area2 = new Vote();
 		Vote area3 = new Vote();
+
 	}
 }
--- Java/basic/src/thread/Vote_class.java
+++ Java/basic/src/thread/Vote_class.java
@@ -1,34 +1,32 @@
 package thread;
 
 public class Vote_class {
+
 	public static void main(String[] args) {
 		Runnable voteCounter = () -> {
 			int percent = 0;
 			String name = Thread.currentThread().getName();
-
 			while (percent < 100) {
 				int rate = (int)(Math.random() * 5) + 1;
-
-				if (percent > 100) {
+				if ((percent + rate) > 100) {
 					rate = 100 - percent;
-					percent = 100;
 				}
 				percent += rate;
 
-				System.out.printf("%5s 개표울: %d%% (개표증가율 : %d%%) %s%n", name, percent, rate, "*".repeat(percent));
+				System.out.printf("%5s 개표율: %3d%% (개표증가율: %d%%) %s%n", name, percent, rate, "*".repeat(percent));
 
 				try {
 					Thread.sleep((long)(1000 * Math.random()));
 				} catch (InterruptedException e) {
-					System.out.println("Interrupted!");
+					System.out.println("Interrupted!!");
 					return;
 				}
 			}
 		};
 
-		Thread t1 = new Thread("제1 지역구");
-		Thread t2 = new Thread("제2 지역구");
-		Thread t3 = new Thread("제3 지역구");
+		Thread t1 = new Thread(voteCounter, "제1지역구");
+		Thread t2 = new Thread(voteCounter, "제2지역구");
+		Thread t3 = new Thread(voteCounter, "제3지역구");
 		t1.start();
 		t2.start();
 		t3.start();
