bb4d113f19250c80c330f2478ca47c1e89b0c938
Kunbam7 <kunh9905@gmail.com>
2025-07-22 17:02:07 +0900

    refactoring with spring

---
+++ spring/demo/src/main/java/com/hana7/demo/LoginController.java
@@ -0,0 +1,11 @@
+package com.hana7.demo;
+
+public class LoginController {
+	public String loginForm() {
+		return  "<form>"
+			+ "<form>"
+			+ "<input name='email'>"
+			+ "<button type='submit'>Login In</button>"
+			+ "</form>";
+	}
+}
