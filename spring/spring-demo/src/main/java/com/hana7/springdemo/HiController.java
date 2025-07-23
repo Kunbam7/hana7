91e1dbb48d3ca641187e4ed6a2f443c461ecd23c
Kunbam7 <kunh9905@gmail.com>
2025-07-23 11:35:15 +0900

    mapping

--- spring/spring-demo/src/main/java/com/hana7/springdemo/HiController.java
+++ spring/spring-demo/src/main/java/com/hana7/springdemo/HiController.java
@@ -1,6 +1,7 @@
 package com.hana7.springdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
@@ -13,5 +14,17 @@ public class HiController {
 		log.debug("Debug={}", name);
 		log.info("Hi INFO");
 		return "Hi, " + name;
+	}
+
+	@GetMapping(value = "/hi/{time}", params = "q=1")
+	public String hiName1(@PathVariable("time") String time) {
+		log.debug("hiName={}", time);
+		return "1: Good " + time;
+	}
+
+	@GetMapping(value = "/hi/{time}", params = "q=2")
+	public String hiName2(@PathVariable("time") String time) {
+		log.debug("hiName={}", time);
+		return "2: Good " + time;
 	}
 }
