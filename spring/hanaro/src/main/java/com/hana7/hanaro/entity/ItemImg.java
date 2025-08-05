ddcc96933338925177bf23fbc55368f5bd0ffb09
Kunbam7 <kunh9905@gmail.com>
2025-08-05 09:39:29 +0900

    upload file

--- spring/hanaro/src/main/java/com/hana7/hanaro/entity/ItemImg.java
+++ spring/hanaro/src/main/java/com/hana7/hanaro/entity/ItemImg.java
@@ -1,4 +1,13 @@
 package com.hana7.hanaro.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class ItemImg {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String imgRoute;
 }
--- spring/spring-demo/src/main/java/com/hana7/springdemo/jpa/controller/MemberController.java
+++ spring/spring-demo/src/main/java/com/hana7/springdemo/jpa/controller/MemberController.java
@@ -2,28 +2,51 @@ package com.hana7.springdemo.jpa.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.hana7.springdemo.jpa.dto.MemberDTO;
 import com.hana7.springdemo.jpa.dto.SearchCond;
+import com.hana7.springdemo.jpa.dto.UploadRequestDTO;
 import com.hana7.springdemo.jpa.service.MemberService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping("/members")
 public class MemberController {
 	private final MemberService service;
+	@Value("${upload.path}")
+	private String uploadPath = "src/main/resources";
 
 	public MemberController(MemberService service) {
 		this.service = service;
+	}
+
+	@Tag(name = "file upload")
+	@Operation(summary = "Upload POST Member")
+	@PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public String upload(UploadRequestDTO dto) {
+		log.info("upfileDTO={}", dto);
+		if (dto.getFiles() != null) {
+			dto.getFiles().forEach(MultipartFil file -> {
+				String orgFrame = file.getOrga
+			});
+		}
+
+		return "File Upload!";
 	}
 
 	@GetMapping()
